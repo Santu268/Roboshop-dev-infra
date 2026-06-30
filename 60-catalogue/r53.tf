d62a6bb506bf04181785c19682fc47765e090dbc
Santosh <rampalli268@gmail.com>
2026-06-30 10:30:45 +0530

    infra dev

--- 60-catalogue/r53.tf
+++ 60-catalogue/r53.tf
@@ -1,8 +1,7 @@
-resource "aws_route53_record" "catalogue" {
-  zone_id = local.zone_id
-  name    = "catalogue.backend-alb-${var.env}.${local.domain_name}"
-  type    = "A"
-  ttl     =  1
-  records = [aws_instance.catalogue.private_ip]
-}
-
+# resource "aws_route53_record" "catalogue" {
+#   zone_id = local.zone_id
+#   name    = "catalogue.backend-alb-${var.env}.${local.domain_name}"
+#   type    = "A"
+#   ttl     =  1
+#   records = [aws_instance.catalogue.private_ip]
+# }
